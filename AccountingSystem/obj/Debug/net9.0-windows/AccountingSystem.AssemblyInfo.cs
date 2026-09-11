4cf44fd1f2843d07a623983ae39ecb7d02d9ce3f
MahdiMohammadi2004 <mahdikingok99@gmail.com>
2026-09-11 14:22:34 +0330

    test

--- AccountingSystem/obj/Debug/net9.0-windows/AccountingSystem.AssemblyInfo.cs
+++ AccountingSystem/obj/Debug/net9.0-windows/AccountingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d7506883b0cf9f446f6ba2978338a55d9f3b562")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
